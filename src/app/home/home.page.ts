df03d0e25d19e5442880cfb660b81651bdf7aa69
Rodrigo Heredia <rodrigoh1205@gmail.com>
2020-11-19 12:37:26 -0400

    Successfully connection with Firebase

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+// Import AngularFirestore to make Queries.
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  doc: any;
 
-  constructor() {}
+  constructor(
+    private firestore: AngularFirestore
+  ) {}
+
+  ngOnInit(){
+    // CALL FIRESTORE DOCUMENT AND SAVE IT IN OUR DOC VARIABLE.
+    this.firestore.doc('/Productos/Carne').valueChanges().subscribe(res => {
+      this.doc = res;
+      console.log('Doc retrieved', this.doc);
+    });
+  }
 
 }
